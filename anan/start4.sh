bf680c40d899b826b901aa5741bcd3ce197f0d02
dogaatac <dogaatac1905@gmail.com>
2024-04-19 03:43:27 +0300

    1x

--- anan/start4.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "$(realpath "${BASH_SOURCE[0]:-$0}")")"
-
-./client -w 7-8  -d /mnt/7/* -d /mnt/8/*   -s /root/cache/7-8/ --no-plotting  -4 --skip-test -v --r-threads 256 -a xch1zpntvxk4507cjpjnwu8ay2shyyqmt0rzmn4luv5jar44h737la6q6fl8l4 
